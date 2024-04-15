60fd80ea330472ca7954995e908aed6117597d72
Tauan Camargo <lukyhenson@gmail.com>
2024-04-15 18:48:29 -0300

    feat: refactor exercises

--- src/exercises/moveZeroes.283.spec.ts
+++ src/exercises/moveZeroes.283.spec.ts
@@ -1,19 +1,3 @@
-// old solution
-// export function moveZeroes(nums: number[]): void {
-// 	const zeroList: number[] = [];
-// 	const numList: number[] = [];
-// 	for (const num of nums) {
-// 		if (num === 0) {
-// 			zeroList.push(num);
-// 		} else {
-// 			numList.push(num);
-// 		}
-// 	}
-//   nums.length = 0;
-//   nums.push(...numList, ...zeroList);
-// }
-
-// Improved solution
 describe("moveZeroes", () => {
 	function moveZeroes(numArr: number[]): number[] {
 		const numFiltered = [...numArr].filter((num) => num > 0);
