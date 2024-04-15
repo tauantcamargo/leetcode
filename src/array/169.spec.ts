541a6dd8bbeb24e1c6b6e7a85df30bb540332bd6
Tauan Camargo <lukyhenson@gmail.com>
2024-04-15 17:15:26 -0300

    feat: leetcode 169 majority element

---
+++ src/array/169.spec.ts
@@ -0,0 +1,27 @@
+describe("test", () => {
+	function majorityElement(nums: number[]): number {
+		let count = 1;
+		let element = nums[0];
+		let i = 1;
+
+		while (i < nums.length) {
+			if (nums[i] !== element) {
+				count--;
+			} else {
+				count++;
+			}
+
+			if (count < 0) {
+				element = nums[i];
+				count = 0;
+			}
+			i++;
+		}
+
+		return element;
+	}
+
+	it("should return 5", () => {
+		expect(majorityElement([6, 5, 5])).toStrictEqual(5);
+	});
+});
